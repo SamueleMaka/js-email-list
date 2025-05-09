ac103eb32cfd64e7fd8c817df9dcaa0c0ded8382
SamueleMaka <samuele.maka.imt@gmail.com>
2025-05-09 17:10:56 +0200

    feat: carica mail solo quando sono 10

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@ const resetButtonEl = document.getElementById("new-mails");
 resetButtonEl.addEventListener("click", resetMails);
 
 function generateEmails(){
+    const generateEmailsArr = [];
     for(let i=0; i<10; i++){
         axios
         .get(
             "https://flynn.boolean.careers/exercises/api/random/mail"
         )
         .then((resp) => {
-            emailListEl.innerHTML += `<li> ${resp.data.response} </li>`
+           generateEmailsArr.push(resp.data.response);
+           if(i===9){
+            for(let j=0; j<10; j++) emailListEl.innerHTML += `<li>${generateEmailsArr[j]}</li>`;
+           }
         });
     }
     
